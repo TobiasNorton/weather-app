37e2ba4dce75a3fb0cf84bf128b9c938dd60f45b
Tobias Norton <tnorton20@yahoo.com>
2019-01-31 17:07:18 -0500

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -3,27 +3,54 @@ import './App.css'
 import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      userInput: ''
+    }
+  }
+
+  getUserInput = event => {
+    this.setState({
+      userInput: event.target.value
+    })
+  }
+
   getCurrentWeather = event => {
-    // event.preventDefault()
-    console.log(event.target.value)
-    // axios
-    //   .get(
-    //     `api.openweathermap.org/data/2.5/weather?q=${event.target.value}&APPID=b2726d7825cf3b1588bfa9175ad211bf&units=imperial`
-    //   )
-    //   .then(response => {
-    //     console.log(response.data)
-    //   })
+    event.preventDefault()
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${
+          this.state.userInput
+        }&APPID=b2726d7825cf3b1588bfa9175ad211bf&units=imperial`
+      )
+      .then(response => {})
   }
 
   render() {
     return (
       <div>
+        <nav className="nav-bar">
+          <div className="nav-left" />
+          <div className="nav-right">
+            <a>Home</a>
+            <a>Current Weather</a>
+            <a>5-Day Forecast</a>
+          </div>
+        </nav>
         <h1>Don't Fear the Weather</h1>
 
         <form onSubmit={this.getCurrentWeather}>
           <p>Enter a city to get the current weather.</p>
-          <input type="text" placeholder="Please enter city name or zip code" name="city search" />
-          <button type="submit">Submit</button>
+          <input
+            type="text"
+            placeholder="Please enter city name or zip code"
+            onChange={this.getUserInput}
+          />
+          <button type="submit" onSubmit={this.getCurrentWeather}>
+            Submit
+          </button>
         </form>
       </div>
     )
